0e9f454507ebf476e3840bcb29f42abd2c2c5b60
domsoos <soos.domi@gmail.com>
2022-09-24 12:14:31 -0400

    added team & refs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component'
 import { TeamComponent } from './team/team.component'
+import {RefsComponent } from './refs/refs.component'
 
 const routes: Routes = [
 { path: '', component: LandingComponent, pathMatch: 'full'},
-{ path: 'team', component: TeamComponent, pathMatch: 'full'}];
+{ path: 'team', component: TeamComponent, pathMatch: 'full'},
+{ path: 'refs', component: RefsComponent, pathMatch: 'full'}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/refs/refs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-refs',
+  templateUrl: './refs.component.html',
+  styleUrls: ['./refs.component.css']
+})
+export class RefsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
