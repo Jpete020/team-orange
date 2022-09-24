96eb41918593b6d19857edd71dfed6b3be2bcc85
Jpete020 <98491629+Jpete020@users.noreply.github.com>
2022-09-24 15:02:38 -0400

    fixed toolbar to top

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
 { path: 'refs', component: RefsComponent, pathMatch: 'full'}];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
